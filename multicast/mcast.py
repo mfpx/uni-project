03a6f881e41958e39b62aa8e2fffcf11dbd15324
David Stumbra <david.stumbra@gmail.com>
2022-08-14 00:54:32 +0100

    Multicast class comments

--- multicast/mcast.py
+++ multicast/mcast.py
@@ -8,6 +8,7 @@ sys.path.insert(0, parentdir)
 from encryption.jsoncrypt import CryptoLib
 
 class MulticastTx:
+    """Class for sending network multicast messages"""
     def __init__(self, name: str, node_type: str, mcast_group: tuple) -> None:
         self.name = name
         self.node_type = node_type
@@ -16,14 +17,17 @@ class MulticastTx:
 
 
     def set_sock_timeout(self, timeout: int) -> None:
+        """Sets the socket timeout"""
         self.timeout = timeout
 
 
     def get_sock_timeout(self) -> int:
+        """Gets the socket timeout"""
         return self.timeout
 
 
     def create_socket(self) -> Callable:
+        """Creates a socket and sets its parameters"""
         sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         sock.settimeout(1)
         ttl = struct.pack('b', self.get_sock_timeout())
@@ -32,6 +36,7 @@ class MulticastTx:
 
 
     def __validate_type(self):
+        """Validates the node type"""
         if self.node_type in ("primary", "backup"):
             return True
         else:
@@ -39,6 +44,7 @@ class MulticastTx:
 
     
     def create_message(self, msgdata: dict, password: str | int) -> bytes:
+        """Creates and encodes a message to be sent"""
         msgdata["name"] = self.name
         if self.__validate_type():
             msgdata["node_type"] = self.node_type
@@ -51,7 +57,8 @@ class MulticastTx:
         return base64.b64encode(str(message).encode())
 
 
-    def mcast_message(self, msg: bytes, socket: socket):
+    def mcast_message(self, msg: bytes, socket: socket) -> dict:
+        """Sends a message to the multicast group"""
         sent = socket.sendto(msg, self.mcast_group)
         print(f"Socket returned {sent}")
 
@@ -87,14 +94,20 @@ malformed - packet received, but the payload was not understood (TO DO)
 """
 
 class MulticastRx:
+    """Class for receiving/acknowledging multicast messages"""
     def __init__(self, mcast_group: tuple) -> None:
         self.mcast_group = mcast_group
         self.timeout = 1
 
 
-    def set_timeout(self, timeout: int) -> None:
+    def set_sock_timeout(self, timeout: int) -> None:
         """Sets the socket timeout"""
         self.timeout = timeout
+
+
+    def get_sock_timeout(self) -> int:
+        """Gets the socket timeout"""
+        return self.timeout
 
 
     def create_socket(self) -> socket:
